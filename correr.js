380f039d8d1a054648778331ace71e1d1d888656
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2019-07-13 10:57:12 -0600

    Update correr.js

--- correr.js
+++ correr.js
@@ -210,19 +210,13 @@ function playSong() {
  }
  var titlex = songInfo[num][1];
  var artistx = songInfo[num][2];
- var albumx = songInfo[num][4]
+ var albumx = songInfo[num][4];
+ var getx = songInfo[num][3];
  navigator.mediaSession.metadata = new MediaMetadata({
   title: titlex,
   artist: artistx,
   album: albumx,
-  artwork: [
-     { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-96x96.png',   sizes: '96x96',   type: 'image/png' },
-     { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-128x128.png', sizes: '128x128', type: 'image/png' },
-     { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-     { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-256x256.png', sizes: '256x256', type: 'image/png' },
-     { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-384x384.png', sizes: '384x384', type: 'image/png' },
-     { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
-   ]
+  artwork:  { src: getx, type: 'image/jpg' }, 
  });
 }
 

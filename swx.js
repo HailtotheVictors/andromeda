204c20cc5c083ee44aa19e84b632670a37d9ab52
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2019-07-13 11:13:05 -0600

    Update swx.js

--- swx.js
+++ swx.js
@@ -1,5 +1,6 @@
 const PRECACHE = 'andromeda-cache-v28';
 const RUNTIME = 'runtime';
+const andro_version = 'V28';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [

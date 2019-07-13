791a0df73f2acca39a75d7eb5da4c9c7516e2291
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2019-07-13 10:15:32 -0600

    Update correr.js

--- correr.js
+++ correr.js
@@ -267,15 +267,8 @@ if ('mediaSession' in navigator) {
  navigator.mediaSession.metadata = new MediaMetadata({
   title: songInfo[num][1],
   artist: songInfo[num][2],
-  album: songInfo[num[4],
-   artwork: [
-      { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-96x96.png',   sizes: '96x96',   type: 'image/png' },
-      { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-128x128.png', sizes: '128x128', type: 'image/png' },
-      { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-256x256.png', sizes: '256x256', type: 'image/png' },
-      { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-384x384.png', sizes: '384x384', type: 'image/png' },
-      { src: 'https://hailtothevictors.github.io/andromeda/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
-    ]
+  album: songInfo[num][4],
+  artwork: { src: songInfo[num][3], type: 'image/jpg' }
  });
 }
 

b8f52ecbca4ebdf5694497287df26f83e95c55d3
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2019-11-09 13:49:55 -0700

    Update correr.js

--- correr.js
+++ correr.js
@@ -159,6 +159,7 @@ function changeDesign(x) {
   $("#logo").attr('src','https://hailtothevictors.github.io/andromeda/andro_assets/andro_word_winter_c.png');
   $("#content").css('backgroundImage','https://hailtothevictors.github.io/andromeda/slower_winter.gif');
  }
+ console.log(x);
 }
 
 var songInfo = [];
@@ -343,6 +344,7 @@ function toggleTrack() {
   $("#songDes").delay(200).fadeIn(200);
   showing = "controls";
  }
+ console.log(showing);
 }
 
 function toggleLyr() {

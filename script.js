fb410076d26766f745a34d99424d7b1242a43321
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-02-24 19:46:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -209,12 +209,12 @@ function updateMeta(song) {
 		artist: artistx,
 		album: albumx,
 		artwork: [
-			{ src: 'https://hailtothevictors.github.io/andromeda/icons/icon-96x96.png',   sizes: '96x96',   type: 'image/png' },
-			{ src: 'https://hailtothevictors.github.io/andromeda/icons/icon-128x128.png', sizes: '128x128', type: 'image/png' },
-			{ src: 'https://hailtothevictors.github.io/andromeda/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-			{ src: 'https://hailtothevictors.github.io/andromeda/icons/icon-256x256.png', sizes: '256x256', type: 'image/png' },
-			{ src: 'https://hailtothevictors.github.io/andromeda/icons/icon-384x384.png', sizes: '384x384', type: 'image/png' },
-			{ src: 'https://hailtothevictors.github.io/andromeda/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+			{ src: 'https://hailtothevictors.github.io/andromeda/newicons/icon-96x96.png',   sizes: '96x96',   type: 'image/png' },
+			{ src: 'https://hailtothevictors.github.io/andromeda/newicons/icon-128x128.png', sizes: '128x128', type: 'image/png' },
+			{ src: 'https://hailtothevictors.github.io/andromeda/newicons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+			{ src: 'https://hailtothevictors.github.io/andromeda/newicons/icon-256x256.png', sizes: '256x256', type: 'image/png' },
+			{ src: 'https://hailtothevictors.github.io/andromeda/newicons/icon-384x384.png', sizes: '384x384', type: 'image/png' },
+			{ src: 'https://hailtothevictors.github.io/andromeda/newicons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
 		]
 	});
 }

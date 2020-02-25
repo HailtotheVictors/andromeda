e49b54a8eef5474e8d28ed38caeda71a4d483666
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2020-02-24 19:33:08 -0700

    Create lyrics.js

---
+++ lyrics.js
@@ -0,0 +1,134 @@
+var supported = [1,2,4,7,10,17,19,50];
+var timestamps = [];
+var lyrics = [];
+timestamps[1] = [27,33,41,49,58.5,65,71,79,87,95.5,106,121,129,137.5,145,154.5,161,167,175,183,191,202.5,219,227,240,247];
+lyrics[1] = ["-","-","[Music]","'Cause I'm tired of all this talking","'Cause everything you hear is empty words","But who am I, oh to say this","'Cause everything you hear is just words, yeah",
+"You have to trust in someone, trust in someone, trust in someone","'Cause the truth is","'Cause all I need, is to see you blossom out, blossom out, blossom out",
+"And all I feel, is that I need you to blossom out, blossom out, blossom out","And all I need, is to see you blossom out, blossom out, blossom out",
+"And all I feel, is that I need you to blossom out, blossom out, blossom out","[Music]","And I'm tired of constant sorrow","'Cause everything you get, is just worse",
+"So what's the point of going somewhere","When everyone you see is just nothing but confused","You have to trust in someone, trust in someone, trust in someone",
+"'Cause the truth is","'Cause all I need, is to see you blossom out, blossom out, blossom out","And all I feel, is that I need you to blossom out, blossom out, blossom out",
+"And all I need, is to see you blossom out, blossom out, blossom out","And all I feel, is that I need you to blossom out, blossom out, blossom out",
+"[Music]","'Cause all I need, is to see you blossom out blossom out blossom out","And all I feel, is that I need you to blossom out, blossom out, blossom out",
+"To blossom out, blossom out, blossom out","To blossom out, blossom out, blossom out","-","-"];
+
+timestamps[2] = [4.5,8.5,12.5,15.5,19.5,24.5,28.5,31.5,35.5,39.5,43.5,46.5,51,55,59,63.5,67.5,71,75,79,84,87,92,96.5,100.5,103.5,107,111,115,118.5,123,
+127,131.5,135.5,139,143,147,151,155,159,164.5,166.5,171,175,179.5,183.5,187,191,195,199,203];
+lyrics[2] = ["-","-","Woah","Woah","You bring an energy I've never felt before","Some kind of chemical that reaches through my core","Feels like as far as you and me, I've never had a choice",
+"You feel like home, hmm","You like the opposite of all of my mistakes (hey)","Tear down the biggest walls and put me in my place","I know, that kind of comfortable you cannot replicate",
+"You feel like home, hmm","So if you're asking me","Say, 'Oh, how do you know?'","I don't just feel it in my heart","Don't just feel it in my heart, no","I feel it in my bones",
+"I feel it in my bones","And I know when it cuts deep down through your chest into your soul","That's how you know, that's how I know","I feel it in my bones (hey)",
+"(Hey)","And I know when it cuts deep down through your chest into your soul (hey)","That's how you know, that's how I know","I feel it in my bones","So many people just walk in and out your life (hey)",
+"I'd trade a couple hundred just to get some time back, that's right","'Cause for you and me, I got no alibi","You feel like home, oh","So if you're asking me","Sayin', 'Oh, how do you know?'",
+"I don't just feel it in my heart","Don't just feel it in my heart, no","I feel it in my bones","I feel it in my bones, yeah","And I know when it cuts deep down through your chest into your soul",
+"That's how you know, that's how I know","I feel it in my bones","I feel it in my bones (hey)","I feel it in my bones","Oh, that's how you know, that's how I know",
+"So if you're asking me","Said, 'Boy, how do you know?'","I don't just feel it in my heart","Don't just feel it in my heart, no","I feel it in my bones (hey)","I feel it in my bones, yeah (hey)",
+"And I know when it cuts deep down through your chest into your soul (hey)","That's how you know, that's how I know","I feel it in my bones","I feel it in my bones (hey)",
+"I feel it in my bones (hey)","Oh, that's how you know, that's how I know","I feel it in my bones","-","-"];
+
+timestamps[4] = [5,9.5,13.5,17.5,26,34.5,38.5,43,47,54,58.5,63,67,71,75,79.5,84,89,93.5,102,104,110,118.5,127,131,138.5,145,147,151.5,155.5,160,
+164,168,173.5,177.5,190,194,205,210,214.5,218.5,224.5,227,231.5,235.5,243,247];
+lyrics[4] = ["-","-","Eh-ah, eh-ah, eh-ah, eh-oh","Eh-ah, eh-ah, oh","Eh-ah, eh-ah, eh-ah, eh-oh","Eh-ah, eh-ah, oh","I know that you must feel like facing with ruin, facing with ruin",
+"I messed it up as always, I'm afraid of losing, I'm afraid of losing","I made you thinking oh, so wrong","I made you feeling the worst","And I don't know how to get along",
+"With myself when I'm weakened, when I'm weakened","So let's go back to our cocoon","On the blackened afternoon","I see your heart is bleeding too","Let me bleed instead of you",
+"So let's go back to our cocoon","On the blackened afternoon","I see your heart is bleeding too","Let me bleed instead of you, let me bleed, let me bleed",
+"Eh-ah, eh-ah, let me bleed, let me bleed","Eh-ah, eh-ah, instead of you","I've been always playing","Solid as I'm waving, solid as I'm waving",
+"So I became a liar, I've been underrating, overestimated","I made it really hard for you, hard for you to love me","But there's so many things we could do",
+"And all we need is escaping, escaping","So let's go back to our cocoon","On the blackened afternoon","I see your heart is bleeding too","Let me bleed instead of you",
+"So let's go back to our cocoon","On the blackened afternoon","I see your heart is bleeding too","Let me bleed instead of you, let me bleed, let me bleed",
+"Eh-ah, eh-ah, let me bleed, let me bleed","Eh-ah, eh-ah, instead of you","Eh-ah, eh-ah, oh","Eh-ah, eh-ah","So let's go back to our cocoon","On the blackened afternoon",
+"I see your heart is bleeding too","Let me bleed instead of you","So let's go back to our cocoon","On the blackened afternoon","I see your heart is bleeding too",
+"Let me bleed instead of you, let me bleed, let me bleed","Let me bleed, let me bleed","Let me bleed, now I will bleed instead of you","-","-"];
+
+timestamps[7] = [8,12,16,20,24,28.5,32.5,36.5,40.5,43.5,48.5,52,57,60,65,68.5,74,89.5,93.5,97.5,101.5,105,110,113.5,118,122,125,130,
+133.5,138,141,146,150,155,179,182,186,190,195,198,203,207];
+lyrics[7] = ["-","-","[Music]","When we were young souls","On the junk-yard","Now we are stunned minds","Full of junk-goods","But I feel your heartbeat","Just like mine",
+"I feel your heartbeat","All the time","And we were bound to the citylife","Flashlights when we're falling into the night","Focused on what you feel",
+"Just when you were calling her love that day","And we were bound to the citylife","Flashlights when we're falling into the night","Focused on what you feel",
+"Just when you were calling her love that day","[Music]","When we were young souls","On the junk-yard","Now we are stunned minds","Full of junk-goods",
+"But I feel your heartbeat","Just like mine","I feel your heartbeat","All the time","And we were bound to the citylife","Flashlights when we're falling into the night",
+"Focused on what you feel","Just when you were calling her love that day","And we were bound to the citylife","Flashlights when we're falling into the night",
+"Focused on what you feel","Just when you were calling her love that day","[Music]","And we were bound to the citylife","Flashlights when we're falling into the night",
+"Focused on what you feel","Just when you were calling her love that day","And we were bound to the citylife","Flashlights when we're falling into the night",
+"Focused on what you feel","Just when you were calling her love that day","-","-"];
+
+timestamps[10] = [10.5,15,16,20,21,25,31,35,36,40.,41.5,45.5,51.5,55.5,56.5,62,67.5,72,79,83,84,88,89,93,99,103,104,108,109,113.5,120,125,129.5,137,
+141.5,146.5,151,153.5,156.5,160.5,161.5,165.5,166.5,170.5,175.5,176.5,180.5,181.5,185.5,186.5,190.5,195.5,196.5,200.5,201.5,205.5,206.5,211,215.5,218,220.5,223.5,225.5];
+lyrics[10] = ["-","-","[Music]","I need a dollar dollar, a dollar is what I need","Hey hey","Well I need a dollar dollar, a dollar is what I need","Hey hey","And I said I need dollar dollar, a dollar is what I need",
+"And if I share with you my story would you share your dollar with me","Bad times are comin' and I reap what I don't sow","Hey hey","Well let me tell you somethin' all that glitters ain't gold",
+"Hey hey","It's been a long old trouble long old troublesome road","And I'm looking for some come and help me carry this load","I need a dollar dollar, a dollar is what I need",
+"Hey hey","Well I need a dollar dollar, a dollar is what I need","Well I don't know if I'm walking on solid ground","Cause everything around me is falling down","And all I want - is for someone - to help me",
+"I had a job but the boss man let me go","He said","I'm sorry but I won't be needing your help no more","I said","Please mister boss man I need this job more than you know",
+"But he gave me my last paycheck and he sent me on out the door","Well I need a dollar dollar, a dollar is what I need","Hey hey","Said I need a dollar dollar, a dollar is what I need",
+"Hey hey","And I need a dollar dollar, a dollar is what I need","And if I share with you my story would you share your dollar with me","Well I don't know if I'm walking on solid ground",
+"Cause everything around me is crumbling down","And all I want is for someone to help me","What in the world am I gonna to do tomorrow","Is there someone whose dollar that I can borrow",
+"Who can help me take away my sorrow","Maybe its inside the bottle","Maybe its inside the bottle","I had some good old buddy his names is whiskey and wine","Hey hey",
+"And for my good old buddy I spent my last dime","Hey hey","My wine is good to me it helps me pass the time","And my good old buddy whiskey keeps me warmer than the sunshine",
+"Hey Hey","Your mom of mayhem just a child has got his own","Hey Hey","If god has plans for me I hope it ain't, written in stone","Hey Hey","Because I've been working working myself down to the bone",
+"And I swear on grandpas grave I'll be paid when I come home","Hey Hey","Well I need a dollar dollar, a dollar is what I need","Hey hey","Said need a dollar dollar, a dollar is what I need",
+"Hey hey","Well I need a dollar dollar, a dollar is what I need hey hey","And if I share with you my story would you share your dollar with me","Come on share your dollar with me",
+"Go ahead share your dollar with me","Come on share your dollar give me your dollar","Share your dollar with me","Come on share your dollar with me","-","-"];
+
+timestamps[17] = [22,27,32.5,38,42,43,45,47.5,49,50.5,53,64.5,70,75,80.5,85,87.5,90.5,91.5,93,95.5,107,118,120.5,123,126,128.5,131,134,136.5,138,140.5,143.5,144.5,146,149,160];
+lyrics[17] = ["-","-","[Music]","Run away with me","Lost souls and reverie","Running wild and running free","Two kids, you and me","And I say","Hey, hey hey hey","Living like we're renegades",
+"Hey hey hey","Hey hey hey","Living like we're renegades","Renegades, renegades","Long live the pioneers","Rebels and mutineers","Go forth and have no fear","Come close the end is near",
+"And I say hey, hey hey hey","Living like we're renegades","Hey hey hey","Hey hey hey","Living like we're renegades","Renegades, renegades","[Music]","All hail the underdogs",
+"All hail the new kids","All hail the outlaws","Spielberg's and Kubrick's","It's our time to make a move","It's our time to make amends","It's our time to break the rules",
+"Let's begin","And I say hey, hey hey hey","Living like we're renegades","Hey hey hey","Hey hey hey","Leaving like we're renegades","Renegades, renegades","[Music]","-","-"];
+
+timestamps[19] = [25,29,34,38,43,47,51,55,63,65,67,70,72,76,80,82,84,87,89,93,97,101,119,122,127,131,135,140,144,148,156,158,160,
+163,165,169,173,175,177,180,182,186,190,194,211,213,215,217,219,223,227,229,231,234,236,240,245,248];
+lyrics[19] = ["-","-","[Music]","I want you by my side","So that I never feel alone again","They've always been so kind","But now they've brought you away from here",
+"I hope they didn't get your mind","Your heart is too strong anyway","We need to fetch back the time","They have stolen from us","And I want you",
+"We can bring it on the floor","You've never danced like this before","We don't talk about it","Dancin' on do the boogie all night long",
+"Stoned in paradise, shouldn't talk about it","And I want you","We can bring it on the floor","You've never danced like this before",
+"We don't talk about it","Dancin' on do the boogie all night long","Stoned in paradise, shouldn't talk about it","Shouldn't talk about it","[Music]",
+"Coldest winter for me","No sun is shining anymore","The only thing I feel is pain","Caused by absence of you",
+"Suspense is controlling my mind","I cannot find the way out of here","I want you by my side","So that I never feel alone again","And I want you",
+"We can bring it on the floor","You've never danced like this before","We don't talk about it","Dancin' on do the boogie all night long",
+"Stoned in paradise, shouldn't talk about it","And I want you","We can bring it on the floor","You've never danced like this before",
+"We don't talk about it","Dancin' on do the boogie all night long","Stoned in paradise, shouldn't talk about it","Shouldn't talk about it","[Music]",
+"And I want you","We can bring it on the floor","You've never danced like this before","We don't talk about it","Dancin' on do the boogie all night long",
+"Stoned in paradise, shouldn't talk about it","And I want you","We can bring it on the floor","You've never danced like this before",
+"We don't talk about it","Dancin' on do the boogie all night long","Stoned in paradise, shouldn't talk about it","Shouldn't talk about it","[Music]","-","-"];
+
+timestamps[50] = [7.28,12.6,14.66,17.04,18.2,22.04,24.02,26.12,27.96,31.56,35.66,38.62,42.34,45.64,48.68,51.76,58.22,60.68,64.98,70.38,72.24,74,77.92,79.84,
+81.7,84,85.8,90.06,93.6,95.88,99.4,103.34,105.94,109.78,115.68,119.28,122.58,128.22,130.68,133.12,137.48,140.4,142.58,148,151.42,156.48,
+160.86,163.36,164.86,170.6,172.46,176.68,180.02,182.66,186.3,192.6,196.1,199.5,204.66,207.16,209.96,214.48];
+lyrics[50] = ["-","-","Lately, I've Been, I've Been Thinking","I Want You To Be Happier. I Want You To Be Happier","When The Morning Comes",
+"When We See What We've Become","In The Cold Light of Day","We're A Flame In The Wind Not The Fire That We've Begun","Every Argument",
+"Every Word We Can't Take Back","Cause With All That Has Happened","I think That We Both Know The Way That The Story Ends","Then Only For A Minute",
+"I Want To Change My Mind","Cause This Just Don't Feel Right To Me","I Want To Raise Your Spirits","I Want To See You Smile",
+"Know That Means I'll Have To Leave","[Music]","Know That Means I'll Have To Leave","Lately I've Been, I've Been Thinking",
+"I Want You To Be Happier. I Want You To Be Happier","When The Evening Falls","And I'm Left There With My Thoughts",
+"And The Imagine With You Being With Someone Else","Well It's Eating Me Up Inside","But We Ran Our Course","We Pretended We're Okay",
+"Now If We Jump Together","At Least We Can Swing Far Away From The Wreck We Made","Then Only For A Minute","I Want To Change My Mind",
+"Cause This Just Don't Feel Right To Me","I Wanna Raise Your Spirits","I Want To See You Smile","Know That Means I'll Have To Leave",
+"[Music]","Know That Means I'll Have To Leave","Lately I've Been, I've Been Thinking, I Want You To Be Happier","I Want You To Be Happier",
+"So I'll Goo","I'll Goooo","I Will Go Go Goo","So I'll Goo","I'll Goooo","I Will Go Go Goo","Lately, I've Been, I've Been Thinking",
+"I Want You To Be Happier","Even Though I Might Not Like This","I Think That You'll Be Happier","I Want You To Be Happier","Then Only For A Minute",
+"I Want To Change My Mind","Cause This Just Don't Feel Right To Me","I Wanna Raise Your Spirits","I Want To See You Smile",
+"Know That Means I'll Have To Leave","[Music]","Know That Means I'll Have To Leave","Lately I've Been, I've Been Thinking",
+"I Want You To Be Happier, I Want You To Be Happier","So I'll Goo","I'll Goooo","I Will Go Go Goo"];
+
+timestamps[51] = [5.5,10.3,12.7,15.1,19.5,24.2,28.9,33.8,38.3,39.9,43.1,44.7,46.1,47.5,49.3,52.5,56.3,58.5,60.3,61.4,64.2,66.9,72.6,
+76.5,81.9,86.8,91.3,96.1,101,105.4,107,110.2,111.8,113.4,114.7,116.4,119.6,123.2,125.3,129.8,130.2,134,135.7,137.4,139.1,140.8,
+145.4,148.6,150.1,151.7,153.1,154.9,158.2,161.6];
+
+lyrics[51] = ["-","-","Legs are shaking, hands are aching","Head is pounding, the back is breaking","The more I give, the more they're taking",
+"I'm broken down, there's no mistaking","There's no time now for goodness sakin'","I'm a simple man, but I'm hard to please",
+"Don't mind you making it hard for me","I don't want to be on my own forever","Alone I'm no good, but when we're together",
+"I'm better than ever","When I'm looking at you I'm better in every way","Better than ever","Got a smile like sunshine",
+"Brightens up the darkest of days","Yeah, when we're together","And that's the way I feel and I'm blaming it all on you",
+"'Cause I'm better than ever, better than ever, better than ever","Ain't that the truth","Everybody from the front to the back",
+"From the left to the right","Let me see you just clap your hands now","You know you make me feel good right?",
+"You make me say hey, hey (hey, hey)","Whoa, whoa (whoa, whoa)","You make me say hey, hey (hey, hey)","Come on, whoa (whoa)",
+"I'm a simple man, I got simple needs","But you're the only kind of trouble for me","I wanna ride side by side forever",
+"Alone I'm no good, but when we're together","I'm better than ever","When I'm looking at you I'm better in every way",
+"Better than ever","Got a smile like sunshine","Brightens up the darkest of days","Yeah, when we're together",
+"Well that's the way I feel and I'm blaming it all on you","'Cause I'm better than ever, better than ever, better than ever",
+"Ain't that the truth","Better, better, better, better (alright now)","Better than ever","Better, better, better, better",
+"Better than ever","Better, better, better, better","Better, better, better, better","Better, better, better, better",
+"I'm better than ever","When I'm looking at you I'm better in every way","Better than ever","Got a smile like sunshine",
+"Brightens up the darkest of days","Yeah, when we're together","Well that's the way I feel and I'm blaming it all on you",
+"'Cause I'm better than ever, better than ever, better than ever","Ain't that the truth","-","-"];
